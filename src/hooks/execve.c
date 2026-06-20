a50f6a57903d6fcf02d09e4ab7b4ccdb78036c68
deref3046 <fine4trn@163.com>
2026-06-21 06:15:36 +0800

    fix missing ptrace.h include for current_pt_regs

--- src/hooks/execve.c
+++ src/hooks/execve.c
@@ -8,6 +8,7 @@
 #include <linux/module.h>
 #include <linux/cred.h>
 #include <linux/sched.h>
+#include <linux/ptrace.h>
 #include <linux/uaccess.h>
 #include <linux/string.h>
 #include <asm/unistd.h>
